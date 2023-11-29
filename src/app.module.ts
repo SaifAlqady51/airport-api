e6901d328b4cd8b7b313ac2e2c6a1551ed6010a1
SaifAlqady51 <saifalqady51@gmail.com>
2023-11-29 18:26:24 +0200

    adding drizzle and airport modules to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AirportModule } from './airport/airport.module';
+import { DrizzleModule } from './drizzle/drizzle.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    AirportModule,
+    DrizzleModule,
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
