db169553699677825c7ff873fe5b4475bae37464
SaifAlqady51 <saifalqady51@gmail.com>
2023-11-29 18:25:22 +0200

    creating airport controller

---
+++ src/airport/airport.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateAirportDto } from './dto/create-airport.dto';
+import { AirportService } from './airport.service';
+import { airportsList } from '../../IATACode.js';
+
+@Controller('api/airport')
+export class AirportController {
+  constructor(private readonly airportService: AirportService) {}
+
+  @Get()
+  getSingleData(
+    @Query('airportCode') airportCode?: string,
+    @Query('cityName') cityName?: string,
+  ) {
+    if (cityName) {
+      return this.airportService.getAllAirportsInCity(cityName);
+    }
+    if (airportCode) {
+      return this.airportService.getAirportByCode(airportCode);
+    }
+  }
+
+  @Post()
+  createAirport(@Body() createAirportDto: CreateAirportDto) {
+    return this.airportService.setAirports(createAirportDto);
+  }
+
+}
