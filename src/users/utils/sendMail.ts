5868d825d59ba774c66f2b2e35efc210086390a5
SaifAlqady51 <saifalqady51@gmail.com>
2024-01-03 18:52:39 +0200

    sendMail.ts deleted unnecassary function

--- src/users/utils/sendMail.ts
+++ src/users/utils/sendMail.ts
@@ -1,49 +1,36 @@
 import * as nodemailer from 'nodemailer';
-import * as path from 'path';
-import * as handlebars from 'handlebars'
+import * as handlebars from 'handlebars';
 import { MessageContent } from './messageContent';
 
-const sendMail = async(to:string,randomNumber:number) => {
-
+const sendMail = async (to: string, randomNumber: number) => {
     const transport = nodemailer.createTransport({
-        service: "gmail",
+        service: 'gmail',
         auth: {
             user: process.env.SMTP_EMAIL,
-            pass: process.env.SMTP_PASSWORD
-        }
-    })
+            pass: process.env.SMTP_PASSWORD,
+        },
+    });
 
-    try{
+    try {
         const testResult = await transport.verify();
-    }
-    catch(error){
+    } catch (error) {
         return;
     }
 
-    const handlebarsOption = {
-        viewEngine: {
-            partialsDir: path.resolve('./views/'),
-            defaultLayout: false,
-        },
-        viewPath: path.resolve('./views/'),
-    }; 
-    
 
-    const template = handlebars.compile(MessageContent)
+    const template = handlebars.compile(MessageContent);
 
-    try{
+    try {
         const sendResult = await transport.sendMail({
             from: process.env.SMTP_EMAIL,
             to,
-            subject:"Validate your email",
-            html:template({randomNumber: randomNumber})
-        })
-        console.log(sendResult)
+            subject: 'Validate your email',
+            html: template({ randomNumber: randomNumber }),
+        });
+        console.log(sendResult);
+    } catch (error) {
+        console.log(error);
     }
-    catch(error){
-        console.log(error)
-    }
-}
+};
 
-
-export default sendMail
+export default sendMail;
