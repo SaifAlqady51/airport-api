16d0edcfd51bf999560072c8ee5293fe21a2daf4
SaifAlqady51 <saifalqady51@gmail.com>
2024-01-14 12:05:23 +0200

    added options to enablecors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+};
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.enableCors({ origin: '*', allowedHeaders: '*', credentials: true });
+    app.enableCors(options);
     await app.listen(4000);
 }
 bootstrap();
